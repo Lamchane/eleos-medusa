5d73459123a68f4d5f754fdf58a60f9701aadde3
nathgoutam93 <nathgoutam93@gmail.com>
2025-05-16 12:30:35 +0530

    hotfix

--- src/api/store/shipping-options/route.ts
+++ src/api/store/shipping-options/route.ts
@@ -9,6 +9,8 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
     "shippingOptionService"
   );
 
+  console.log(req.body);
+
   const { addresses } = req.body as unknown as {
     addresses: Record<string, any>[];
   };
@@ -18,12 +20,46 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
       admin_only: false,
     });
 
+    console.log("addresess from rzp:", addresses);
+
+    res.json({
+      addresses: [
+        {
+          id: "0",
+          zipcode: "560000",
+          state_code: "KA",
+          country: "IN",
+          shipping_methods: [
+            {
+              id: "1",
+              description: "Free shipping",
+              name: "Delivery within 5 days",
+              serviceable: true,
+              shipping_fee: 1000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+              cod: true,
+              cod_fee: 1000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+            },
+            {
+              id: "2",
+              description: "Standard Delivery",
+              name: "Delivered on the same day",
+              serviceable: true,
+              shipping_fee: 1000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+              cod: false,
+              cod_fee: 0, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+            },
+          ],
+        },
+      ],
+    });
+
     return res.json({
-      addresses: addresses.map((address: any) => ({
+      addresses: addresses.map((address) => ({
         ...address,
         shipping_methods: options.map((option) => ({
           id: option.id,
           name: option.name,
+          description: "Free Shipping",
           serviceable: true,
           shipping_fee: option.amount,
           cod: option.metadata.cod,
